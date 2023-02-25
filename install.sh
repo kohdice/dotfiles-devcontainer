37a57f440c3cf0774fa63d8b7ec32f94318171ed
kohdice <kohdice@gmail.com>
2023-02-25 18:14:43 +0900

    Remove neovim related settings and files

--- install.sh
+++ install.sh
@@ -8,13 +8,3 @@ curl -o .git-prompt.sh \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 ln -fs ~/dotfiles/.bashrc .
-
-
-# Neovim
-mkdir -p ~/.config
-
-cp -r ~/dotfiles/nvim ~/.config/
-
-#Packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
