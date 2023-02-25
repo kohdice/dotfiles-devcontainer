dbe81c3bff4817995803a729c6f701b4303b077a
kohdice <kohdice@gmail.com>
2023-02-25 16:56:05 +0900

    Add neovim-related code to install.sh.

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ curl -o .git-prompt.sh \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 ln -fs ~/dotfiles/.bashrc .
+
+mkdir -p ~/.config
+
+ln -fs ~/dotfiles/nvim ~/.config/
